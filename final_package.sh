712716f5b28ee2bd764dba8f86b1ae7cfc8db762
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2026-08-25 08:57:16 -0400

    add steps to final package

--- final_package.sh
+++ final_package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export REPO=saoimageds9
+TARFILE=ds9-flatpak-repo-unsigned.tar
 
 if test x$GPG_KEY_ID = x
 then
@@ -7,10 +9,16 @@ then
 	exit 1
 fi
 
-export REPO=saoimageds9
+if test -f ${TARFILE}
+then
+  :
+else
+    echo "Cannot find tar file."
+    exit 1
+fi
 
 mkdir -p "$REPO"
-tar -C "$REPO" -xf ds9-flatpak-repo-unsigned.tar
+tar -C "$REPO" -xf ${TARFILE}
 
 for ref in $(ostree refs --repo="$REPO"); do
     ostree --repo="$REPO" rev-parse "$ref"
@@ -24,5 +32,19 @@ flatpak build-update-repo \
     --gpg-sign="$GPG_KEY_ID" \
     --generate-static-deltas \
     "$REPO"
+
+gpg --batch --export "$GPG_KEY_ID" > "$REPO/saoimageds9.gpg"
+GPG_KEY_B64=$(base64 --wrap=0 < "$REPO/saoimageds9.gpg")
+cat > "$REPO/saoimageds9.flatpakrepo" <<EOF
+[Flatpak Repo]
+Title=SAOImageDS9
+Url=https://ds9.si.edu/beta/flatpak/
+Homepage=https://ds9.si.edu/
+Comment=SAOImageDS9 Flatpak Repository
+Description=Official SAOImageDS9 Flatpak Repository
+GPGKey=${GPG_KEY_B64}
+Nodelete=false
+EOF
+
 
 tar -C "$REPO" -cf saoimageds9.tar .
